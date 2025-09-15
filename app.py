9060bdc2ff229ba0b15308846b45d98c1484c0a6
het4399 <het.sojitra4399@gmail.com>
2025-09-15 17:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2743,5 +2743,5 @@ def get_sitemap_urls(website_url):
     return []
 
 if __name__ == '__main__':
-    logger.info("Starting LLM Text Generator application")
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
